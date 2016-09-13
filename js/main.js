996ffddd1941685100672ab7b1426617066807af
titli1981 <madhumishra084@gmailcom>
2016-09-13 17:21:00 -0400

    used fadeIn and fadeOut function to fadein and fadeout the modal window

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    console.log( "ready!");
+
+$("button").click(function(){
+    $('.modal').fadeIn();
+});
+$("button").click(function(){
+    $('.modal').fadeOut();
+});
+
+
+});
